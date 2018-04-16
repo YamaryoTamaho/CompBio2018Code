c31d27d40674c1b82c203a1cbf08340a76b3ac68
YamaryoTamaho <38201131+YamaryoTamaho@users.noreply.github.com>
2018-04-16 16:29:26 +0900

    kadai2

--- 02/kadai_2.c
+++ 02/kadai_2.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 int main(void){
-  int t,x;
-  FILE *fp;
-  fp=fopen("output.dat","W");
-  fprintf(fp,"%d,%d",0,x0);
-  for(t=)
+  int t=0;
+  double k=100.0,r=1.0,x1,x2;
 
+  FILE *fp;
+  fp=fopen("output.dat","w");
+
+  for(r=1.0;r<3.0;r+=0.1){
+    fprintf(fp,"r=%.1f\n",r);
+
+    x1=10.0;
+    for(t=0;t<50;t++){
+    x2=x1+r*(1.0-x1/k)*x1;
+    fprintf(fp,"%d,%.3f\n",t,x2);
+    x1=x2;
+    }
+  }
+  fclose(fp);
+  return 0;
 }
