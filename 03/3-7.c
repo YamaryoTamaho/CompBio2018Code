db83bcb37e6bcd3a8c8c363031df8a3104c0786c
YamaryoTamaho <38201131+YamaryoTamaho@users.noreply.github.com>
2018-05-14 05:20:34 +0900

    update

--- 03/3-7.c
+++ 03/3-7.c
@@ -9,13 +9,13 @@ int main(void){
   xa=x0;
   r=1.0;
   k=100.0;
-  dt=0.01;
+  dt=0.001;
 
   FILE *fp;
   fp=fopen("3-7.dat","w");
   fprintf(fp,"%f,%f,%f\n",0.0,xa,x);
 
-  for(i=1;i<1000;i++){
+  for(i=1;i<10000;i++){
     xa=k/(1+(k/x0-1)*exp(-r*dt*i));
 
     xx=x+dt*r*(1-x/k)*x;
---
+++ 04/4-5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int i1,i2;
+  double d1,d2;
+
+  i1=5;
+  d1=6.0;
+  i2=(int)d1%i1;
+  d2=(double)i1/d1;
+
+  printf("%d, %f\n",i2,d2);
+
+  return 0;
+}
